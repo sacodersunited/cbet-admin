0079a790b64079a4f87bdf3b17b51834deef6782
Paul <azrael7@gmail.com>
2020-06-17 16:56:53 -0500

    Changed redirect for auth.

--- app/azure-auth/authProvider.js
+++ app/azure-auth/authProvider.js
@@ -1,13 +1,13 @@
-import { MsalAuthProvider, LoginType } from "react-aad-msal";
-import "regenerator-runtime";
+import { MsalAuthProvider, LoginType } from "react-aad-msal"
+import "regenerator-runtime"
 
-const tenant = "cbettenant.onmicrosoft.com";
-const signInPolicy = "B2C_1_CbetAdminSignUpv1";
-const applicationID = process.env.CBET_AZURE_APPID;
-const reactRedirectUri = "http://localhost:8080/";
-const tenantSubdomain = tenant.split(".")[0];
-const instance = `https://${tenantSubdomain}.b2clogin.com/tfp/`;
-const signInAuthority = `${instance}${tenant}/${signInPolicy}`;
+const tenant = "cbettenant.onmicrosoft.com"
+const signInPolicy = "B2C_1_CbetAdminSignUpv1"
+const applicationID = process.env.CBET_AZURE_APPID
+const reactRedirectUri = "https://epic-beaver-ac3d99.netlify.app/"
+const tenantSubdomain = tenant.split(".")[0]
+const instance = `https://${tenantSubdomain}.b2clogin.com/tfp/`
+const signInAuthority = `${instance}${tenant}/${signInPolicy}`
 
 // Msal Configurations
 const signInConfig = {
@@ -21,7 +21,7 @@ const signInConfig = {
     cacheLocation: "sessionStorage",
     storeAuthStateInCookie: true,
   },
-};
+}
 
 // Authentication Parameters
 const authenticationParameters = {
@@ -29,15 +29,15 @@ const authenticationParameters = {
     "https://graph.microsoft.com/Directory.Read.All",
     "https://cbettenant.onmicrosoft.com/api-1/user_impersonation",
   ],
-};
+}
 
 const options = {
   loginType: LoginType.Redirect,
-  tokenRefreshUri: "http://localhost:8080/",
-};
+  tokenRefreshUri: "https://epic-beaver-ac3d99.netlify.app/",
+}
 
 export const signInAuthProvider = new MsalAuthProvider(
   signInConfig,
   authenticationParameters,
   options
-);
+)
