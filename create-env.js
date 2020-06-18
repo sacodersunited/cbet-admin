bafd957ab8a1880bdbecca152b3c832eaf7939d6
Paul <azrael7@gmail.com>
2020-06-18 14:15:13 -0500

    Added other cbetContentCode to create-env.

--- create-env.js
+++ create-env.js
@@ -1,2 +1,2 @@
 const fs = require("fs")
-fs.writeFileSync("./.env", `CBET_AZURE_APPID=${process.env.CBET_AZURE_APPID}\n`)
+fs.writeFileSync("./.env", `CBET_AZURE_APPID=${process.env.CBET_AZURE_APPID}\ncbetContentCode=${process.env.cbetContentCode}\n`)
