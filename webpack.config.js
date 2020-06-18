60277666d1199165c3cc3318d0ee17d5af9f4bab
Paul <azrael7@gmail.com>
2020-06-18 09:35:27 -0500

    Adds react-icons, file-loader, cbetdropzone.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|jpg|gif|mp3|ico|svg)$/,
+        loader: 'file-loader',
+        options: {}
+      }
     ],
   },
   mode: "development",
