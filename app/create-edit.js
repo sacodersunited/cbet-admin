592963ae8c2714bddae0c0c96a4bc392fc4852f8
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-06-17 22:54:33 -0500

    adds Routing - adds starter comps for all categories - adds styles for active links

---
+++ app/create-edit.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CreateEdit({ title }) {
+  return (
+    <>
+      <p>I AM {title}</p>
+      <p>TODO: {title} go here</p>
+    </>
+  )
+}
---
+++ app/jobs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Jobs({ title }) {
+  return (
+    <>
+      <p>I AM {title}</p>
+      <p>TODO: {title} go here</p>
+    </>
+  )
+}
