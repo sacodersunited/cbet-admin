214b86539ec4cf02f19a36cca7eee5ec119a251f
Paul <azrael7@gmail.com>
2020-07-18 12:38:46 -0500

    Added dates to cbetContent object.

--- app/blogs.js
+++ app/blogs.js
@@ -24,7 +24,7 @@ export default function Blogs(props) {
 
     let cbetContent = {
       ID: post.Id, // number
-      ContentTitle: '', // string
+      ContentTitle: post.Title, // string
       Description: '', // HTML for blog
       Thumbnail: '',
       PartnerName: '', // string
@@ -33,8 +33,8 @@ export default function Blogs(props) {
       Status: false, // string
       CbetCategory: post.CbetCategory_Id, // number
       Link: '', // string
-      StartDate: '', // date
-      EndDate: '', // date
+      StartDate: post.StartDate, // date
+      EndDate: post.EndDate, // date
       Location: '', // string
       Tags: 'one,two', // string
       Featured: false, // bool
